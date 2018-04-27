159a99998dbae523e894c125dda1986d55b0205a
Ashley Winnop <arwinnop@gmail.com>
2018-04-26 21:39:44 -0600

    submission final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,98 @@
+console.log ("broad city guessing game");
+
 var words = [
-    "al dente dentist", 
-    "bed bath and beyond", 
-    "bingo bronson", 
-    "kirk steele", 
-    "natures pocket", 
-    "worldwide bloodstream",
+    "abbi", 
+    "ilana",
+    "jaime",
+    "jeremy",
+    "lincoln", 
+    "bevers", 
+    "bedbugs", 
+    "witches", 
+    "friendiversary",
     "soulstice",
     "val"
 ];
 
 var wordSelect;
+var wordLength;
 var underscores = [];
-var answer;
+var answer = false;
 
 var wins = 0;
-var points = 10;
+var points;
 var lettersGuessed = [];
+var UNDERSCORE = " _ ";
 
+var startAudio = new Audio("assets/images/BroadCityOpening.mp3");
+// var loseAudio = new Audio("../images/cat meow - sound effect.mp3");
+// var winAudio = new Audio("../images/Broad City YAS Supercut.mp3");
+
+function wordDisplay() {
+    var display = "";
+    for (var i = 0; i < underscores.length; i++) {
+        display = display + " " + underscores[i];
+    }
+
+    document.querySelector("#word").innerHTML = display;
+}
 
 // Function to start game and randomly select word 
 function gameStart() {
+    startAudio.play();
     wordSelect = words[Math.floor(Math.random() * words.length)];
-    var wordLength = wordSelect.length;
+    wordLength = wordSelect.length;
+    underscores = [];
+    lettersGuessed = [];
+    points = 10;
+    document.querySelector("#points").innerHTML = points;
+    document.querySelector("#emptyInside").innerHTML = "";
+    document.querySelector("#lettersGuessed").innerHTML = lettersGuessed;
 
     for (var i = 0; i < wordLength; i++) {
-        underscores.push("__ ");
+        underscores.push(UNDERSCORE);
     }
+
+    wordDisplay();
 }
 
 document.onkeyup = function(event) {
     var userGuess = event.key.toLowerCase();
 
-    lettersGuessed.push(userGuess);
-
     answer = false;
 
-    for (var i = 0; i < wordLength; i++) {
-        if  (userGuess === wordSelect.charAt(i)) {
-            underscores[i] = userGuess;
-            answer = true;
+    if(lettersGuessed.indexOf(userGuess) == -1) {
+        lettersGuessed.push(userGuess);
+        for (var i = 0; i < wordLength; i++) {
+            if  (userGuess === wordSelect.charAt(i)) {
+                underscores[i] = userGuess;
+                answer = true;
+            }
         }
     }
+
+    document.querySelector("#lettersGuessed").innerHTML = lettersGuessed;
     
     if (answer != true) {
         points--;
+        console.log(points);
+        document.querySelector("#points").innerHTML = points;
     }
 
-    if (underscores = 0 && points > 0) {
+
+    if (underscores.indexOf(UNDERSCORE) == -1 && points > 0) {
         wins++;
-        document.querySelector("#win").innerHTML = "YAS KWEEN!";
-    } else ((underscores > 0) && (points = 0)); {
-        document.querySelector("#lose").innerHTML = "BAD TRIP!";
+        document.querySelector("#wins").innerHTML = wins;
+        document.querySelector("#emptyInside").innerHTML = "YAS KWEEN!";
+        setTimeout(gameStart, 3000);
+        // snd.play(winAudio);
+    } else if ((underscores.indexOf(UNDERSCORE) > -1) && (points == 0)) {
+        document.querySelector("#emptyInside").innerHTML = "BAD TRIP!";
+        setTimeout(gameStart, 3000);
+        // snd.play(loseAudio);
     }
 
+    wordDisplay();
 }
+
+gameStart();
